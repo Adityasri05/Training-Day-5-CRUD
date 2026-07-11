82526d457bc1b80b5ef689e85363850b66194427
Aditya Srivastav <adityakrsrivastav2005@gmail.com>
2026-07-11 11:01:05 +0530

    Update login API endpoint to deployment URL

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
                 password: password,
             };
 
-            const response = await fetch("/api/auth/login", {
+            const response = await fetch("https://training-day-8-mywood-deployment.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
